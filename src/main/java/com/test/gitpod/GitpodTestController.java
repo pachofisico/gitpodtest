1acede902f4df5ca362e4877a03f224fb795a796
Francisco Martinez <francisco_martinez@scoutt.co>
2024-09-02 17:12:30 -0500

    First version:  use send get request to /gitpod/test/Hello

---
+++ src/main/java/com/test/gitpod/GitpodTestController.java
@@ -0,0 +1,24 @@
+package com.test.gitpod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+        import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/gitpod")
+@Validated
+public class GitpodTestController {
+
+
+    @GetMapping("/test/{name}")
+    public ResponseEntity<String> register(@Validated @PathVariable String name){
+        String response = "Test " + name;
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+}
---
+++ src/test/java/com/test/gitpod/GitpodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.gitpod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
