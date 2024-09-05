25fc1676b1c3d5ddcba9a6c7495c4efafc2b9406
Francisco Martinez <pachofisico@hotmail.com>
2024-09-05 20:00:33 +0000

    Updated setjava21.sh

--- setjava21.sh
+++ setjava21.sh
@@ -1,6 +1,10 @@
 sudo apt-get update
 sudo apt-get install -y openjdk-21-jdk
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-21-openjdk-amd64/bin/java 1
+sudo update-alternatives --set java /usr/lib/jvm/java-21-openjdk-amd64/bin/java
+unset JAVA_HOME
 export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
-source ~/.bashrc
 export PATH=$JAVA_HOME/bin:$PATH
-java --version
+source ~/.bashrc
+echo $JAVA_HOME
+java -version
